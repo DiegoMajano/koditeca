819fe1a9a1d02e630779385265892eed0466481d
DiegoMajano <dmajano302@gmail.com>
2025-01-04 22:23:22 -0600

    agregando estructura de proyecto

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>KodiTeca</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <div>
+                <img src="" alt="">
+                <h2>KodiTeca</h2>
+            </div>
+            <ul>
+                <li>Inicio</li>
+                <li>Libros</li>
+                <li>Prestamos</li>
+                <li>Contacto</li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <div>
+
+        </div>
+    </main>
+</body>
+</html>
